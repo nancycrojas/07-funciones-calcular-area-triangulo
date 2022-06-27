d5f8a0eccbdceb42e6c66c8423defb517ed1bffc
nancycrojas <nanclarisaok@gmail.com>
2022-06-26 22:11:07 -0300

    Funciones - ejercicio 07

---
+++ app.js
@@ -0,0 +1,13 @@
+//calcularAreaTriangulo(base, altura)
+//Crear una función calcularAreaTriangulo que tome como argumentos la base y la altura de un triángulo y devuelva el área del mismo
+
+//calcularAreaTriangulo(3, 4) // 6
+//calcularAreaTriangulo(5, 6) // 40 el resultado es 15
+
+const calcularAreaTriangulo = (base, altura) => {
+//function calcularAreaTriangulo(base, altura){
+    return (base * altura) /2;
+  }
+
+  calcularAreaTriangulo(3, 4);
+  calcularAreaTriangulo(5, 6);
